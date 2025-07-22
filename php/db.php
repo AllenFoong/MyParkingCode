7eaefe35ec23986b2320083cdda1c31fe9676d44
LoongDengQi <dengqi96@gmail.com>
2025-07-22 15:43:08 +0800

    remove files
    Initial state

--- php/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect("sql308.infinityfree.com", "if0_39118857", "myparking123", "if0_39118857_parkingdb");
-
-if (!$conn) {
-    die("Database connection failed: " . mysqli_connect_error());
-}
-?>
